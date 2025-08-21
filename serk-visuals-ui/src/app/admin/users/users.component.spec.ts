f8339aaaa07fb3c6668d73e01b67428de2a46c95
Daniel Haile <daniel27haile@gmail.com>
2025-08-20 22:40:06 -0700

    commit changes...

--- serk-visuals-ui/src/app/admin/users/users.component.spec.ts
+++ serk-visuals-ui/src/app/admin/users/users.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MediaComponent } from './media.component';
+import { UsersComponent } from './users.component';
 
-describe('MediaComponent', () => {
-  let component: MediaComponent;
-  let fixture: ComponentFixture<MediaComponent>;
+describe('UsersComponent', () => {
+  let component: UsersComponent;
+  let fixture: ComponentFixture<UsersComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MediaComponent]
+      imports: [UsersComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MediaComponent);
+    fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

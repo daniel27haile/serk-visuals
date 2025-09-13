ff5e3006cdbde7188be6adf0ffb4ab17400d20a0
Daniel Haile <daniel27haile@gmail.com>
2025-09-13 11:45:00 -0700

    commit changes on header, footer and update gallery

--- serk-visuals-ui/src/app/header/header.component.ts
+++ serk-visuals-ui/src/app/header/header.component.ts
@@ -1,29 +1,34 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd, RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  Router,
+  NavigationEnd,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  standalone: true,
   imports: [RouterLink, RouterLinkActive],
 })
 export class HeaderComponent {
   isMenuOpen = false;
 
   constructor(private router: Router) {
-    // Close menu on route change (mobile)
+    // Close mobile menu on navigation
     this.router.events
       .pipe(filter((e) => e instanceof NavigationEnd))
       .subscribe(() => (this.isMenuOpen = false));
   }
 
-  toggleMenu() {
+  toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  closeMenu() {
+  closeMenu(): void {
     this.isMenuOpen = false;
   }
 }

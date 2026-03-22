d4a7a879a534f8b1c4fd5dcba0f2386805cbf43c
Daniel Haile <daniel27haile@gmail.com>
2026-03-21 21:50:09 -0700

    S3 CloudFront with OAC

--- serk-visuals-ui/environment/environment.prod.ts
+++ serk-visuals-ui/environment/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://serk-visuals-api.onrender.com'
+  apiBaseUrl: 'https://serk-visuals-api.onrender.com',
+  CDN_URL: 'd19gnizvws4w1d.cloudfront.net'
 };

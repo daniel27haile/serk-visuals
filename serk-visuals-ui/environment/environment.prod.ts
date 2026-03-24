a6510aa39ce22b9baafbb0e2fbd3f4276776a8be
Daniel Haile <daniel27haile@gmail.com>
2026-03-23 19:20:09 -0700

    adding CDN_URL on prod env

--- serk-visuals-ui/environment/environment.prod.ts
+++ serk-visuals-ui/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://serk-visuals-api.onrender.com',
-  CDN_URL: 'd19gnizvws4w1d.cloudfront.net'
+  CDN_URL: 'https://d19gnizvws4w1d.cloudfront.net'
 };

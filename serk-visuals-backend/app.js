900bfb90fa7c1528f17aad2dbac715f781ba2410
Daniel Haile <daniel27haile@gmail.com>
2026-07-08 18:24:46 -0700

    fix: port missmatch...

--- serk-visuals-backend/app.js
+++ serk-visuals-backend/app.js
@@ -96,7 +96,7 @@ app.use((err, _req, res, _next) => {
   res.status(500).json({ error: "Internal server error" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 (async () => {
   try {
